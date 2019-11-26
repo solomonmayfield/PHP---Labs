6568042486be30bad8ea6cb189221b74e6ba54b8
solomonmayfield <37204315+solomonmayfield@users.noreply.github.com>
2019-11-26 09:35:10 -0800

    Add files via upload

---
+++ l3-3.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ * demo_postback_nohtml.php is a single page web application that allows us to request and view 
+ * a customer's name
+ *
+ * This version uses no HTML directly so we can code collapse more efficiently
+ *
+ * This page is a model on which to demonstrate fundamentals of single page, postback 
+ * web applications.
+ *
+ * Any number of additional steps or processes can be added by adding keywords to the switch 
+ * statement and identifying a hidden form field in the previous step's form:
+ *
+ *<code>
+ * <input type="hidden" name="act" value="next" />
+ *</code>
+ * 
+ * The above live of code shows the parameter "act" being loaded with the value "next" which would be the 
+ * unique identifier for the next step of a multi-step process
+ *
+ * @package ITC281
+ * @author Solomon Mayfield<smayfio1@gmail.com>
+ * @version 1.1 2011/10/11
+ * @link http://www.newmanix.com/
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
+ * @todo finish instruction sheet
+ * @todo add more complicated checkbox & radio button examples
+ */
+
+# '../' works for a sub-folder.  use './' for the root  
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+ 
+/*
+$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
+$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
+$config->metaRobots = 'no index, no follow';
+$config->loadhead = ''; #load page specific JS
+$config->banner = ''; #goes inside header
+$config->copyright = ''; #goes inside footer
+$config->sidebar1 = ''; #goes inside left side of page
+$config->sidebar2 = ''; #goes inside right side of page
+$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
+$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
+*/
+
+//END CONFIG AREA ----------------------------------------------------------
+
+# Read the value of 'action' whether it is passed via $_POST or $_GET with $_REQUEST
+if(isset($_REQUEST['act'])){$myAction = (trim($_REQUEST['act']));}else{$myAction = "";}
+
+switch ($myAction) 
+{//check 'act' for type of process
+	case "display": # 2)Display user's name!
+	 	showBallplayers();
+	 	break;
+	default: # 1)Ask user to enter their ball player data 
+	 	showForm();
+}
+
+function showForm()
+{# shows form so user can enter their name.  Initial scenario
+	get_header(); #defaults to header_inc.php	
+	
+	echo 
+	'<script type="text/javascript" src="' . VIRTUAL_PATH . 'include/util.js"></script>
+	<script type="text/javascript">
+		function checkForm(thisForm)
+		{//check form data for valid info
+			if(empty(thisForm.name,"Please Enter Your Players Name")){return false;}
+			if(empty(thisForm.team,"Please Enter Your Players team")){return false;}
+			if(empty(thisForm.homer,"Please Enter Your Players homer")){return false;}
+			return true;//if all is passed, submit!
+		}
+	</script>
+	<h3 align="center">' . smartTitle() . '</h3>
+	<p align="center">Please enter your name</p> 
+	<form action="' . THIS_PAGE . '" method="post" onsubmit="return checkForm(this);">
+		<table align="center">
+			<tr>
+				<td align="right">
+					Name
+				</td>
+				<td>
+					<input type="text" name="name" /><font color="red"><b>*</b></font> <em>(alphabetic only)</em>
+				</td>
+			</tr>
+			<tr>
+				<td align="right">
+					team
+				</td>
+				<td>
+					<input type="text" name="team" /><font color="red"><b>*</b></font> <em>(alphabetic only)</em>
+				</td>
+			</tr>
+			<tr>
+				<td align="right">
+					homer
+				</td>
+				<td>
+					<input type="text" name="homer" /><font color="red"><b>*</b></font> <em>(alphabetic only)</em>
+				</td>
+			</tr>
+			<tr>
+				<td align="center" colspan="2">
+					<input type="submit" value="Please Enter Your Name"><em>(<font color="red"><b>*</b> required field</font>)</em>
+				</td>
+			</tr>
+		</table>
+		<input type="hidden" name="act" value="display" />
+	</form>
+	';
+	get_footer(); #defaults to footer_inc.php
+}
+
+function showBallplayers()
+{#form submits here we show entered name
+	get_header(); #defaults to footer_inc.php
+	
+	echo '<pre>';
+	var_dump($_POST);
+	echo '</pre>';
+	die;
+	/*
+	dumpDie($_POST);
+	*/
+	if(!isset($_POST['name']) || $_POST['name'] == '')
+	{//data must be sent	
+		feedback("No form data submitted"); #will feedback to submitting page via session variable
+		myRedirect(THIS_PAGE);
+	}  
+	
+	if(!isset($_POST['team']) || $_POST['team'] == '')
+	{//data must be sent	
+		feedback("No form data submitted"); #will feedback to submitting page via session variable
+		myRedirect(THIS_PAGE);
+	}  
+	
+	if(!isset($_POST['homer']) || $_POST['homer'] == '')
+	{//data must be sent	
+		feedback("No form data submitted"); #will feedback to submitting page via session variable
+		myRedirect(THIS_PAGE);
+	}  
+	
+	
+	
+	
+	if(!ctype_alnum($_POST['name']))
+	{//data must be alphanumeric only	
+		feedback("Only letters and numbers are allowed.  Please re-enter your name."); #will feedback to submitting page via session variable
+		myRedirect(THIS_PAGE);
+	}
+	
+	$name = strip_tags($_POST['name']);# here's where we can strip out unwanted data
+	$team = strip_tags($_POST['team']);# here's where we can strip out unwanted data
+	$homer = (int)$_POST['homer'];# here's where we can strip out unwanted data
+	$myBallPlayers[] = new BallPlayer($name,$team,$homer);
+	//dumpDie($myBallPlayer);
+	echo '<h3 align="center">' . smartTitle() . '</h3>';
+	if(!isset($_SESSION)){session_start();}/*if session exists add to it. if not start new*/
+	if(isset($_SESSION['myBallPlayers']))
+	{//add to existing session variable
+		$myBallPlayers = $_SESSION['myBallPlayers'];
+	}else{//start new session
+		$myBallPlayers = array();
+	}
+	
+	$myBallPlayers[] = new BallPlayer($name,$team,$homer);
+	
+	$_SESSION['myBallPlayers'] = $myBallPlayers; 
+	
+	foreach($myBallPlayers as $player)
+	{
+		echo "Name: " . $player->name . "<br />";
+		echo "Team: " . $player->name . "<br />";
+		echo "Homer: " . $player->homer . "<br />";
+	}
+	
+	
+	dumpDie($_SESSION['myBallPlayers']);
+	
+	//unset($_SESSION['myBallPlayers']);
+	echo '<p align="center"><a href="' . THIS_PAGE . '">RESET</a></p>';
+	get_footer(); #defaults to footer_inc.php
+}
+
+	class BallPlayer {
+
+	//static vars allow data to accrue as totals  
+	private static $TotalHomers = 0; //Will Store Total Homers
+	private static $TotalPlayers = 0; //Will Store Total BallPlayers
+	private $name = '';  //private so user can't change during program
+ 	private $team = '';
+ 	private $homers = 0;  //allowing change to homers during runtime would mess up static variable
+    function __construct($name,$team,$homers)
+	{//constructor sets stage by adding data to an instance of the object
+		if($name == ""){die(trigger_error("name property of BallPlayer object does not allow empty string", E_USER_ERROR));}	
+	    if($team == ""){die(trigger_error("team property of BallPlayer object does not allow empty string", E_USER_ERROR));}	
+		if(!is_int($homers) || $homers < 0){die(trigger_error("homers property of BallPlayer object must be a positive whole number (integer)", E_USER_ERROR));}
+	
+		$this->name = $name;
+		$this->team = $team;
+		$this->homers = $homers;
+		self::$TotalHomers += $homers; //add homers to total
+		self::$TotalPlayers++;  //increment total ballplayers
+	}
+	
+	public static function averageHomers()
+	{//static function allows us to see the average homers of all players
+		return 	number_format(self::$TotalHomers/self::$TotalPlayers,2);	
+	}
+	
+	public function toString()
+	{//allows us to see all properties of the object
+		return 	" name: " . $this->name . " team: " . $this->team . " homers: " . $this->homers;	
+	}
+	
+	public static function totalHomers()
+	{//static function to allow read only access to total homers
+		return self::$TotalHomers;
+	}
+
+	public static function totalPlayers()
+	{//static function to allow read only access to total players
+		return self::$TotalPlayers;
+	}		
+
+}
+
+
